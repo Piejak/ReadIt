5051001fbc419ee3316a92361811f7f5f4e845b7
Brian Piejak <Brian.Piejak@gmail.com>
2016-03-21 19:23:08 -0400

    Clean up imports

--- app/src/main/java/com/bpiejak/readit/MainActivity.java
+++ app/src/main/java/com/bpiejak/readit/MainActivity.java
@@ -1,19 +1,10 @@
 package com.bpiejak.readit;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
