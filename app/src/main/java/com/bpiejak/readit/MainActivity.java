163decb57731e3e8eae06caa67133330974017bb
Brian Piejak <Brian.Piejak@gmail.com>
2016-03-21 18:39:34 -0400

    Switch to MaterialDrawer library

--- app/src/main/java/com/bpiejak/readit/MainActivity.java
+++ app/src/main/java/com/bpiejak/readit/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.mikepenz.materialdrawer.DrawerBuilder;
+
 public class MainActivity extends AppCompatActivity {
 
     private String[] mPlanetTitles = {"test", "another test", "one more"};
@@ -27,56 +29,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.tool_bar);
-
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
-        //Set adapter for list view
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, R.layout.drawer_layout_item,
-                mPlanetTitles);
-        mDrawerList.setAdapter(itemsAdapter);
-        //set list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-        mTitle = mDrawerTitle = getTitle();
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                mToolbar, R.string.drawer_open, R.string.drawer_close) {
-
-        };
-    }
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener{
-        @Override
-        public void onItemClick(AdapterView parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
-
-    /**
-     * Swaps fragments in the main content view
-     * @param position determines which fragment to insert into main content view
-     */
-    public void selectItem(int position) {
-        //new fragment and specify which one based on position
-        Fragment fragment = new Fragment();
-        Bundle args = new Bundle();
-        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-        fragment.setArguments(args);
-
-        //Insert fragment, replacing existing fragment
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.content_frame, fragment)
-                .commit();
-
-        mDrawerList.setItemChecked(position, true);
-        setTitle(mPlanetTitles[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
-    }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getActionBar().setTitle(mTitle);
+        new DrawerBuilder().withActivity(this).build();
     }
 }
