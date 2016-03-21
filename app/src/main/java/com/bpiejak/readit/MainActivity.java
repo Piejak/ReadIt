10e96167962b4e57cf533fbbd9b69fdbf399e0ec
Brian Piejak <Brian.Piejak@gmail.com>
2016-03-21 18:55:04 -0400

    Style toolbar

--- app/src/main/java/com/bpiejak/readit/MainActivity.java
+++ app/src/main/java/com/bpiejak/readit/MainActivity.java
@@ -7,21 +7,19 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String[] mPlanetTitles = {"test", "another test", "one more"};
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private CharSequence mTitle;
-    private CharSequence mDrawerTitle;
-    private ActionBarDrawerToggle mDrawerToggle;
     private Toolbar mToolbar;
 
     @Override
@@ -29,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.tool_bar);
-        new DrawerBuilder().withActivity(this).build();
+        setSupportActionBar(mToolbar);
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName(R.string.item1);
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName(R.string.item2);
+        Drawer drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(mToolbar)
+                .addDrawerItems(
+                    item1,
+                    item2
+                )
+                .build();
     }
 }
