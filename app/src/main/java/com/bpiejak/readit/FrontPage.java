7dd05b4bdcefad05f8958e725ccf1290657d0736
Brian Piejak <Brian.Piejak@gmail.com>
2016-03-24 00:57:34 -0400

    Add pull to refresh functionality

--- app/src/main/java/com/bpiejak/readit/FrontPage.java
+++ app/src/main/java/com/bpiejak/readit/FrontPage.java
@@ -2,18 +2,22 @@ package com.bpiejak.readit;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by brian_000 on 3/21/2016.
  */
 public class FrontPage extends Fragment {
     private static final int DATASET_COUNT = 30;
+
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     protected RecyclerView mRecyclerView;
     protected RecyclerView.LayoutManager mLayoutManager;
@@ -30,7 +34,17 @@ public class FrontPage extends Fragment {
         mAdapter = new CustomAdapter(mDataset);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
-        Log.i("FrontPage", "onCreateView is being run");
+
+        swipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.front_page_refresh);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(getContext(), "Refreshed", Toast.LENGTH_SHORT).show();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        swipeRefreshLayout.setColorSchemeColors(R.color.colorAccent, R.color.primary);
 
         return rootView;
     }
